41ad6df6c7d8603c9b9a34bbf08f26d572fbea3f
iwata-motonori <motonori.iwata@dena.com>
2016-01-08 15:40:00 +0900

    install ansible2 and elasticsearch

--- PlayAnsible.sh
+++ PlayAnsible.sh
@@ -13,7 +13,11 @@ cd ${PROVISIONING_HOME}/provisioning > /dev/null 2>&1
 pa_echo "Doing common playbook"
 HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-playbook playbook.yml -i hosts
 
+# via ansible2
+ansible-playbook playbook_after_brew.yml -i hosts
 
+ANSIBLE_VERSION_MAJOR=$(echo $(ansible --version) | sed -E "s/.*[^0-9]([0-9]+\.[0-9]+\.[0-9]+).*/\1/g" | cut -d"." -f1)
+echo "ANSIBLE_VERSION_MAJOR=${ANSIBLE_VERSION_MAJOR}"
 if [ $ANSIBLE_VERSION_MAJOR -eq 1 ]
 then
   DEFAULTS_PLAYBOOK=playbook_defaults_v1.yml
@@ -24,4 +28,7 @@ fi
 pa_echo "Doing playbook is ${DEFAULTS_PLAYBOOK}, which is depending ansible version"
 ansible-playbook ${DEFAULTS_PLAYBOOK} -i hosts
 
-osascript -e 'display notification "Successfull prease restart or re-login" with title "osx-provisioning"'
+source $HOME/.zshenv
+ansible-playbook playbook_llenv.yml -i hosts
+
+osascript -e 'display notification "Successfull please restart or re-login" with title "osx-provisioning"'
