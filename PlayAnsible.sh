a7a1f6489773040ec506fc5354ced118bda9b6a9
iwata <121048+iwata@users.noreply.github.com>
2019-09-04 14:52:27 +0900

    install Homebrew via shell script instead of Ansible

--- PlayAnsible.sh
+++ PlayAnsible.sh
@@ -8,10 +8,14 @@ function pa_echo () {
   echo [${TASK_NAME}] $1
 }
 
+
 cd ${PROVISIONING_HOME}/provisioning > /dev/null 2>&1
 
 pa_echo "Doing playbook for Mac defaults"
 ansible-playbook playbook_defaults.yml
+
+pa_echo "Install Homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 pa_echo "Doing common playbook"
 env HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-playbook playbook.yml
